bb972feefe0115ffab14d2022a6068a72e4355a5
FrankWaffle <f3191252@gmail.com>
2021-09-03 14:03:03 +0800

    Update 2021-09-03T06:03:01.654Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,19 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+$(document).ready(function () {
+  var swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      }
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true
+    }
+  });
 });
 //# sourceMappingURL=all.js.map
